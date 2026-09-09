2bfb8c8f1f381c040e719ab7ef9398f4cfa77d25
Reza Ali <rezaalim767@gmail.com>
2026-09-09 19:20:38 +0700

    [Tugas P-2] setup route admin plus screenshot

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,9 @@ Route::resource('members', MemberController::class);
 Route::resource('loans', LoanController::class);
 Route::put('/loans/{id}/kembalikan', [LoanController::class, 'kembalikan'])
     ->name('loans.kembalikan');
+
+Route::prefix('admin')->group(function () {
+    Route::get('/info', function () {
+        return 'Halaman Informasi Admin';
+    }); 
+});
